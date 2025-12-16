a4eca0b90d2c64eca03c1df0b99d2c392ae19793
Saye Vikram <87774705+SayeVikram@users.noreply.github.com>
2025-12-15 19:09:57 -0500

    added comments

--- class_builder.py
+++ class_builder.py
@@ -12,6 +12,7 @@ class Encoding:
             raise KeyError("Name of channel not specified")
         self.current = string
         return self
+        #setting a current field so that when we call field that current field is updated
     
     def field(self, fieldstr):
         self.encoding[self.current] = fieldstr
@@ -19,6 +20,7 @@ class Encoding:
     
     def to_dict(self):
         return self.encoding
+    #to_dict to receiving the encoding attribute
     
 class visualization:
     def __init__(self):
@@ -27,6 +29,7 @@ class visualization:
             "mark": None,
             "encoding": {}
         }
+        #default initializer
 
     def mark(self, stringchannel):
         if stringchannel not in ["bar", "line", "point", "area"]:
@@ -37,12 +40,14 @@ class visualization:
     def add_encoding(self, encoding):
         self._viz["encoding"].update(encoding.to_dict())
         return self
+        #updating the dictionary
 
     def to_dict(self):
         return self._viz
 
 
 class layout:
+
     def __init__(self):
         self._layout = {
             "type": "layout",
@@ -53,10 +58,12 @@ class layout:
 
     def direction(self, direction: str):
         self._layout["direction"] = direction
+        #appropriate setter for direction
         return self
 
     def gap(self, gap: str):
         self._layout["gap"] = gap
+        #setter for gap
         return self
 
     def add_child(self, node):
@@ -93,14 +100,19 @@ example_two_test = (
         )
     )
 )
-
+#creating the object according to the specifications
 
 
 def main():
-    file_path = "python_generated_test_two.json"
+    file_path_two = "python_generated_test_two.json"
+    file_path_one = "python_gen_test.json"
+    #Two unit tests that are generate the json files necessary
 
-    with open(file_path, "w") as json_file:
+    with open(file_path_two, "w") as json_file:
         json.dump(example_two_test.to_dict(), json_file, indent=4)
+
+    with open(file_path_one, "w") as json_file:
+        json.dump(example_one_test.to_dict(), json_file, indent=4)
 
 
 
